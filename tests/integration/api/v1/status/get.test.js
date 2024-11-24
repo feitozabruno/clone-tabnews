69bf9fdcfa1b66f4f721fda384c9647eb7bfc938
Bruno Feitoza <brunofeitozanascimento@gmail.com>
2024-11-24 20:57:07 -0300

    refactor: use `orchestrator.clearDatabase()` in tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,7 @@
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices;
+  await orchestrator.waitForAllServices();
 });
 
 describe("GET /api/v1/status", () => {
